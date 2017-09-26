5594ac522757e538cf17a034545b9ed719fd653b
Logan Christensen <dapizzamon22@gmail.com>
2017-09-26 13:51:34 -0600

    tons of custom CSS

--- scripts/controllers/main-controller.js
+++ scripts/controllers/main-controller.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
   $('body').on("click tap", "input[type='button']#settings-button", function(){
     $("#settings").addClass("expanded");
-  })
+  });
+  $('body').on("click tap", "#settings .minimize", function(){
+    $('#settings').toggleClass("expanded");
+  });
 });
