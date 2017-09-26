bc87bf08f7e457aa146c9786b390170aaeabb516
Logan Christensen <dapizzamon22@gmail.com>
2017-09-26 12:14:35 -0600

    added range input enhancements

--- scripts/plugins.js
+++ scripts/plugins.js
@@ -13,4 +13,35 @@ $(document).ready(function(){
   $('input[type="checkbox"]').each(function(){
     convertInputToToggler($(this));
   });
+  $('input[type="range"]').each(function(){
+    convertRange($(this));
+  });
 });
+/* END TOGGLERS*/
+
+/* SPICING UP RANGE INPUT */
+function convertRange(input){
+  input.before("<div class='rangeLabel' />");
+  input.on("input", function(){
+    var unit = $(this).attr("data-unit");
+    var label = $(this).prev(".rangeLabel");
+    label.text("");
+    switch($(this).attr("data-representation")){
+      case "unit":
+      var string = "";
+      for (i = 0; i < $(this).val(); i++){
+        string += unit;
+      }
+      label.text(string);
+      break;
+      default:
+      case "number":
+        var string = $(this).val() + unit;
+        label.text(string);
+      break;
+
+    }
+  });
+  input.trigger("input");
+}
+/* END RANGE INPUT */
