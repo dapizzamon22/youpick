7c3cf924fe7beb9a62a3c256c5e2314f10bdc7e3
Logan Christensen <dapizzamon22@gmail.com>
2017-09-26 14:51:32 -0600

    falling food animation

--- scripts/plugins.js
+++ scripts/plugins.js
@@ -1,49 +1,79 @@
 /* TURNING CHECKBOXES INTO TOGGLERS */
-function convertInputToToggler(input){
+function convertInputToToggler(input) {
   var element = $("<div class='toggler'><div class='switch' /></div>");
   input.hide();
   input.before(element);
-  element.on("tap click", function(){
+  element.on("tap click", function() {
     $(this).toggleClass("checked");
     $(this).next("input[type='checkbox']").click();
   })
 }
 
-$(document).ready(function(){
-  $('input[type="checkbox"]').each(function(){
+$(document).ready(function() {
+  $('input[type="checkbox"]').each(function() {
     convertInputToToggler($(this));
   });
-  $('input[type="range"]').each(function(){
+  $('input[type="range"]').each(function() {
     convertRange($(this));
   });
 });
 /* END TOGGLERS*/
 
 /* SPICING UP RANGE INPUT */
-function convertRange(input){
+function convertRange(input) {
   input.before("<div class='rangeLabel' />");
-  input.on("input", function(){
+  input.on("input", function() {
     var unit = $(this).attr("data-unit");
     var label = $(this).prev(".rangeLabel");
     label.remove();
     label = $("<div class='rangeLabel' />");
     $(this).before(label);
-    switch($(this).attr("data-representation")){
+    switch ($(this).attr("data-representation")) {
       case "unit":
-      var string = "";
-      for (i = 0; i < $(this).val(); i++){
-        string += unit;
-      }
-      label.text(string);
-      break;
+        var string = "";
+        for (i = 0; i < $(this).val(); i++) {
+          string += unit;
+        }
+        label.text(string);
+        break;
       default:
       case "number":
         var string = $(this).val() + unit;
         label.text(string);
-      break;
+        break;
 
     }
   });
   input.trigger("input");
 }
 /* END RANGE INPUT */
+
+/* FALLING FOOD */
+
+var gravity = 25;
+
+var Food = function(svg, x, y, angle, angularVelocity) {
+  var elem = $("<object type=\"image/svg+xml\" data=\"" + svg + "\" class=\"food\">FOOD!</object>");
+  elem.css({
+    'left': x + "px",
+    'top': y + "px",
+    'transform': 'rotate('+ angle +'deg)'
+  });
+  $('.foodFall').append(elem);
+
+}
+$(document).ready(function() {
+  setTimeout(function(){
+    if ($('.foodFall').length > 0) {
+      var cheese = new Food("img/CheeseWheel.svg", -50, -375, 90, 15);
+      var cheese = new Food("img/Ciabatta.svg", 50, -300, 90, 15);
+      var cheese = new Food("img/Huevo.svg", -75, -250, 90, 15);
+      var cheese = new Food("img/RibEye.svg", 75, -450, 90, 15);
+      var cheese = new Food("img/Wieners.svg", 0, -250, 90, 15);
+      var cheese = new Food("img/FrenchBread.svg", 0, -275, 90, 15);
+    }
+  }, 1000);
+});
+
+
+/* END FALLING FOOD*/
