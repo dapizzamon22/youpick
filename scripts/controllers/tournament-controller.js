d5fc7a3b095304edb18c77817ae603e7648c26e2
Logan Christensen <dapizzamon22@gmail.com>
2017-09-27 08:30:49 -0600

    i don't remember

--- scripts/controllers/tournament-controller.js
+++ scripts/controllers/tournament-controller.js
@@ -17,19 +17,16 @@ $(window).on("load", function() {
           radiusInMeters = loadSettings().radius * 1609.34;
           var placesURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + lng + "&rankby=distance&type=restaurant&key=" + apikey;
           var restaurants = [];
-
-          pageResult(restaurants, placesURL, null, function () {
-            console.log(restuarants);
-           for (i = 0; i < restaurants.length; i++) {
-           $("#results-list").append("<li>" + restaurants[i].name + "</li>");
-           }
-           setRestaurantList(restaurants);
-           switchFragment("tournament");
-           });
+          pageResult(restaurants, placesURL, null, function() {
+            for (i = 0; i < restaurants.length; i++) {
+              $("#results-list").append("<li>" + restaurants[i].name + "</li>");
+            }
+            setRestaurantList(restaurants);
+            switchFragment("tournament");
+          });
 
           console.log("results:", restaurants.results);
           setRestaurantList(restaurants.results);
-          switchFragment("tournament");
           console.log("GETTING RESTAURANTS");
         }
       });
@@ -38,37 +35,38 @@ $(window).on("load", function() {
   });
 
   function pageResult(container, url, token = null, callback) {
-      var newURL;
-      if (token !== null) {
-          console.log("TOKEN PRESENT");
-          newURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=" + token + "&key=" + apikey;
-      } else {
-          console.log("NO TOKEN");
-          newURL = url + "";
+    var newURL;
+    if (token !== null) {
+      console.log("TOKEN PRESENT");
+      newURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=" + token + "&key=" + apikey;
+    } else {
+      console.log("NO TOKEN");
+      newURL = url + "";
+    }
+    console.log(newURL);
+    $.post("scripts/php/get-remote.php", {
+      url: newURL
+    }, function(response) {
+      response = JSON.parse(response);
+      if (response.status == "INVALID_REQUEST") {
+        console.log("INVALID REQUEST", newURL);
+        console.log(response);
+        return;
       }
-      console.log(newURL);
-      $.post("scripts/php/get-remote.php", {url: newURL}, function (response) {
-          response = JSON.parse(response);
-          if (response.status == "INVALID_REQUEST") {
-              console.log("INVALID REQUEST", newURL);
-              console.log(response);
-              return;
-          }
-          console.log(response);
-          var locations = response.results;
-          for (i = 0; i < locations.length; i++) {
-              container.push(locations[i]);
-          }
-          if (response.hasOwnProperty("next_page_token")) {
-              console.log("NOT NULL!", response.next_page_token);
-              setTimeout(function () {
-                  pageResult(container, url, response.next_page_token, callback);
-              }, 2000);
-          } else {
-              callback();
-          }
-
-      });
+      console.log(response);
+      var locations = response.results;
+      for (i = 0; i < locations.length; i++) {
+        container.push(locations[i]);
+      }
+      if (response.hasOwnProperty("next_page_token")) {
+        console.log("NOT NULL!", response.next_page_token);
+        setTimeout(function() {
+          pageResult(container, url, response.next_page_token, callback);
+        }, 2000);
+      } else {
+        callback();
+      }
+    });
   }
 
   var loadingMessage = ["Loading...", "Adding pepperoni's...", "Checking prices...", "taking a small potty break...", "Browsing YouTube...", "Getting distracted..."];
@@ -82,3 +80,12 @@ $(window).on("load", function() {
     });
   }, 4000);*/
 });
+
+function getRestaurantList() {
+  console.log("list:", localStorage.getItem("restaurants"));
+  return JSON.parse(localStorage.getItem("restaurants"));
+}
+
+function setRestaurantList(newList) {
+  localStorage.setItem("restaurants", JSON.stringify(newList));
+}
